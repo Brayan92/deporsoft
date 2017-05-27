f8a2e1a04aeef0eb2ea6ae2ea196427d3e325dbb
Brayan camacho <bcamacho92.bc@gmail.com>
2017-05-26 21:21:22 -0500

    ajuste footer

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+# config valid only for current version of Capistrano
+
+set :repo_url, 'git@github.com:Brayan92/deporsoft.git'
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+set :user, 'rails'
+set :application, 'deporsoft'
+set :rails_env, 'production'
+server 'deporsoft.cloudsoft.com.co', user: "#{fetch(:user)}", roles: %w{app db web}, primary: true
+set :deploy_to,       "/home/#{fetch(:user)}/#{fetch(:application)}"
+set :pty, true
+
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/puma.rb')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :config_example_suffix, '.example'
+set :config_files, %w{config/database.yml config/secrets.yml}
+set :puma_conf, "#{shared_path}/config/puma.rb"
+
+namespace :deploy do
+  before 'check:linked_files', 'config:push'
+  before 'check:linked_files', 'puma:config'
+  before 'check:linked_files', 'puma:nginx_config'
+  before 'deploy:migrate', 'deploy:db:create'
+  after 'puma:smart_restart', 'nginx:restart'
+end
