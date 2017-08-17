26738394cffd1c5ba09c0a7b6585959b02d2a963
Brayan camacho <bcamacho92.bc@gmail.com>
2017-08-16 23:09:46 -0500

    ajustes vistas y mensajes

--- app/controllers/championships_controller.rb
+++ app/controllers/championships_controller.rb
@@ -10,25 +10,49 @@ class ChampionshipsController < ApplicationController
   # GET /championships/1
   # GET /championships/1.json
   def show
+    @days = { 1 => "Lunes",
+                2 => "Martes",
+                3 => "Miercoles",
+                4 => "Jueves",
+                5 => "Viernes",
+                6 => "Sabado",
+                7 => "Domingo",
+         }
   end
 
   # GET /championships/new
   def new
     @championship = Championship.new
+    @days = { 1 => "Lunes",
+                2 => "Martes",
+                3 => "Miercoles",
+                4 => "Jueves",
+                5 => "Viernes",
+                6 => "Sabado",
+                7 => "Domingo",
+         }
   end
 
   # GET /championships/1/edit
   def edit
+    @days = { 1 => "Lunes",
+                2 => "Martes",
+                3 => "Miercoles",
+                4 => "Jueves",
+                5 => "Viernes",
+                6 => "Sabado",
+                7 => "Domingo",
+         }
   end
 
   # POST /championships
   # POST /championships.json
   def create
+    set_days_game
     @championship = Championship.new(championship_params)
-
     respond_to do |format|
       if @championship.save
-        format.html { redirect_to '/championships', notice: 'Championship was successfully created.' }
+        format.html { redirect_to '/championships', notice: 'Campeonato creado correctamente.' }
         format.json { render :show, status: :created, location: @championship }
       else
         format.html { render :new }
@@ -41,8 +65,10 @@ class ChampionshipsController < ApplicationController
   # PATCH/PUT /championships/1.json
   def update
     respond_to do |format|
+    set_days_game
+    puts params
       if @championship.update(championship_params)
-        format.html { redirect_to @championship, notice: 'Championship was successfully updated.' }
+        format.html { redirect_to @championship, notice: 'Campeonato actulizado correctamente.' }
         format.json { render :show, status: :ok, location: @championship }
       else
         format.html { render :edit }
@@ -65,6 +91,9 @@ class ChampionshipsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_championship
       @championship = Championship.find(params[:id])
+    end
+    def set_days_game
+      params[:championship][:game_days] = params[:days].join(',')
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
