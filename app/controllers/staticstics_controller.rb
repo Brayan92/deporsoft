f05625b5535e863040731a4d1de789fa1a4d0542
brayan camacho <bcamacho92.bc@gmail.com>
2017-10-12 15:03:04 +0000

    ajuste ordenamientos

--- app/controllers/staticstics_controller.rb
+++ app/controllers/staticstics_controller.rb
@@ -5,17 +5,17 @@ class StaticsticsController < ApplicationController
 	    @scores = []
 
 	    if params[:championship_id].present?
-	       @scores =  Report::TableScore.score.where(championship_id:params[:championship_id]).order('puntos desc')
+	       @scores =  Report::TableScore.score.where(championship_id:params[:championship_id]).order('puntos DESC')
 	    end
     end
 
     def table_players_goals
 	    @events = []
 	    if params[:championship_id].present? && params[:event_id].present?
-	       @events = Report::PlayersGoals.events.where(championship_id:params[:championship_id],event_id:params[:event_id])
+	       @events = Report::PlayersGoals.events.where(championship_id:params[:championship_id],event_id:params[:event_id]).order('cantidad DESC')
 	    else
-        @events = Report::PlayersGoals.events.where(championship_id:params[:championship_id],3).order('cantidad desc')
-      end
+               @events = Report::PlayersGoals.events.where(championship_id:params[:championship_id],event_id:3).order('cantidad DESC')
+            end
     end
   end
 
