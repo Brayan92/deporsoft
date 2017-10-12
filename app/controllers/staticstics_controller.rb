83f4108e02d864030569d60cd532d0a5cc3330f2
Fernando Freire Gómez <fernando.freire@esinergia.co>
2017-10-12 10:01:02 -0500

    ajuste tabla de eventos

--- app/controllers/staticstics_controller.rb
+++ app/controllers/staticstics_controller.rb
@@ -12,9 +12,11 @@ class StaticsticsController < ApplicationController
     def table_players_goals
 	    @events = []
 	    if params[:championship_id].present? && params[:event_id].present?
-	       @events = Report::PlayersGoals.events.where(championship_id:params[:championship_id],event_id:params[:event_id])
+	        sql = 'select p.id as id , concat(p.first_name," ",p.last_name) as jugador,e.name as evento,e.id,count(e.id)as cantidad from details_matches inner join players p on p.id=player_id inner join events e on e.id=event_id where e.id = 3 group by p.id order by cantidad DESC;'
+			@events = ActiveRecord::Base.connection.execute(sql)	    	
 	    else
-        @events = Report::PlayersGoals.events.where(championship_id:params[:championship_id],3).order('cantidad desc')
+	        sql = 'select p.id as id , concat(p.first_name," ",p.last_name) as jugador,e.name as evento,e.id,count(e.id)as cantidad from details_matches inner join players p on p.id=player_id inner join events e on e.id=event_id where e.id = 3 group by p.id order by cantidad DESC;'
+			@events = ActiveRecord::Base.connection.execute(sql)	    	
       end
     end
   end
