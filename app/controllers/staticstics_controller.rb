1334544a3fd5d097848710fbefd8df7a44394e2d
Fernando Freire Gómez <fernando.freire@esinergia.co>
2017-10-12 10:22:49 -0500

    ajuste tabla de eventos

--- app/controllers/staticstics_controller.rb
+++ app/controllers/staticstics_controller.rb
@@ -9,7 +9,7 @@ class StaticsticsController < ApplicationController
     def table_players_goals
 	    @events = []
 	    if params[:championship_id].present? && params[:event_id].present?
-	        sql = 'select t.name as equipo,p.id as id , concat(p.first_name," ",p.last_name) as nombre,e.name as evento,e.id,count(e.id)as cantidad from details_matches d inner join players p on p.id=d.player_id inner join events e on e.id=d.event_id inner join teams t on t.id = d.team_id where e.id = 3 group by p.id,t.id order by cantidad DESC'
+	        sql = 'select t.name as equipo,p.id as id , concat(p.first_name," ",p.last_name) as nombre,e.name as evento,e.id,count(e.id)as cantidad from details_matches d inner join players p on p.id=d.player_id inner join events e on e.id=d.event_id inner join teams t on t.id = d.team_id where e.id = ' << params[:event_id].to_s << ' event_id group by p.id,t.id order by cantidad DESC'
 			@events = ActiveRecord::Base.connection.execute(sql)	    	
 	    else
 	        sql = 'select t.name as equipo,p.id as id , concat(p.first_name," ",p.last_name) as nombre,e.name as evento,e.id,count(e.id)as cantidad from details_matches d inner join players p on p.id=d.player_id inner join events e on e.id=d.event_id inner join teams t on t.id = d.team_id where e.id = 3 group by p.id,t.id order by cantidad DESC'
